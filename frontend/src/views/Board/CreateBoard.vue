d52a417400caf587e7ff575476ad113ab2a13164
whaudg98 <whaudg98@naver.com>
2021-02-08 03:01:21 +0900

    [feat] 게시글 작성 폼 생성

--- frontend/src/views/Board/CreateBoard.vue
+++ frontend/src/views/Board/CreateBoard.vue
@@ -1,102 +1,83 @@
 <template>
-  <div>
+  <div class='d-flex flex-column'>
     <base-header class="pb-6 pb-8 pt-5 pt-md-8 bg-gradient-default">
     </base-header>
 
-    <b-container style="background: white; border-radius: 1rem;" class="py-4 mt-4">
-      <b-row>
-        <h1 class="ml-3 mb-0">제목</h1>
-      </b-row>
-      <hr class="my-2">
-      <b-row>
-        <b-col cols="1" class="pr-0 mb-2">img 들어갈 곳</b-col>
-        <b-col>
-          <h3>
-            작성자 : 닉네임
-          </h3>
-          <h5>
-            0000년 00월 00일 00:00작성 / 0000년 00월 00일 00:00 수정됨
-          </h5>
-        </b-col>
-      </b-row>
-      <hr class="my-2">
-      <b-row>
-        <p class="px-3">
-          ㅁ넝러미넝라ㅣ머ㅣㄴ어리멍러ㅣ
-          ㅁ넝러미넝라ㅣ머ㅣㄴ어리멍러ㅣ
-          ㅁ넝러미넝라ㅣ머ㅣㄴ어리멍러ㅣ
-          ㅁ넝러미넝라ㅣ머ㅣㄴ어리멍러ㅣ
-          ㅁ넝러미넝라ㅣ머ㅣㄴ어리멍러ㅣ
-          ㅁ넝러미넝라ㅣ머ㅣㄴ어리멍러ㅣ
-          ㅁ넝러미넝라ㅣ머ㅣㄴ어리멍러ㅣ
-          ㅁ넝러미넝라ㅣ머ㅣㄴ어리멍러ㅣ
-          ㅁ넝러미넝라ㅣ머ㅣㄴ어리멍러ㅣ
-          ㅁ넝러미넝라ㅣ머ㅣㄴ어리멍러ㅣ
-          ㅁ넝러미넝라ㅣ머ㅣㄴ어리멍러ㅣ
-          ㅁ넝러미넝라ㅣ머ㅣㄴ어리멍러ㅣ
-          ㅁ넝러미넝라ㅣ머ㅣㄴ어리멍러ㅣ
-          ㅁ넝러미넝라ㅣ머ㅣㄴ어리멍러ㅣ
-          ㅁ넝러미넝라ㅣ머ㅣㄴ어리멍러ㅣ
-          ㅁ넝러미넝라ㅣ머ㅣㄴ어리멍러ㅣ
-          ㅁ넝러미넝라ㅣ머ㅣㄴ어리멍러ㅣ
-          ㅁ넝러미넝라ㅣ머ㅣㄴ어리멍러ㅣ
-          ㅁ넝러미넝라ㅣ머ㅣㄴ어리멍러ㅣ
-          ㅁ넝러미넝라ㅣ머ㅣㄴ어리멍러ㅣ
-          ㅁ넝러미넝라ㅣ머ㅣㄴ어리멍러ㅣ
-          ㅁ넝러미넝라ㅣ머ㅣㄴ어리멍러ㅣ
-          ㅁ넝러미넝라ㅣ머ㅣㄴ어리멍러ㅣ
-          ㅁ넝러미넝라ㅣ머ㅣㄴ어리멍러ㅣ
-          ㅁ넝러미넝라ㅣ머ㅣㄴ어리멍러ㅣ
-          ㅁ넝러미넝라ㅣ머ㅣㄴ어리멍러ㅣ
-          ㅁ넝러미넝라ㅣ머ㅣㄴ어리멍러ㅣ
-          ㅁ넝러미넝라ㅣ머ㅣㄴ어리멍러ㅣ
-          ㅁ넝러미넝라ㅣ머ㅣㄴ어리멍러ㅣ
-          ㅁ넝러미넝라ㅣ머ㅣㄴ어리멍러ㅣ
-        </p>
-      </b-row>
-      <b-row align-h="end" class="my-2">
-        <i class="far fa-trash-alt fa-lg mr-3"></i>
-        <i class="far fa-edit fa-lg mr-3"></i>
-      </b-row>
-      <b-row>
-        <i class="far fa-thumbs-up fa-2x ml-3"><span class="h3">좋아요</span></i>
-        <i class="far fa-thumbs-down fa-2x ml-3"><span class="h3">싫어요</span></i>
-      </b-row>
-    </b-container>
 
-    <b-container style="background: white; border-radius: 1rem;" class="py-4 mt-4">
-      <b-row>
-        <h2 class="ml-3 mb-0">댓글(숫자)</h2>
-      </b-row>
-      <hr class="my-2">
-      <b-row>
-        <b-col cols="8" align-self="center">
-          [img] 작성자 : 닉네임
-        </b-col>
-        <b-col>
-          <h5>
-            0000년 00월 00일 00:00
-          </h5>
-          <h5>
-            0000년 00월 00일 00:00
-          </h5>
-        </b-col>
-      </b-row>
-      <b-row>
-        <p class="px-3">
-          ㅁ넝러미넝라ㅣ머ㅣㄴ어리멍러ㅣ
-        </p>
-      </b-row>
-      <b-row align-h="end" class="my-2">
-        <i class="far fa-thumbs-up fa-2x ml-3"><span class="h3">숫자</span></i>
-      </b-row>
-    </b-container>
+    <div class="p-4 bg-secondary">
+      <b-input placeholder="제목" class="form-control-alternative"/>
+    </div>
+
+    <div>
+      <b-card-form>
+        <editor
+          ref="toastuiEditor"
+          :initialValue="editorText"
+          :options="editorOptions"
+          height="500px"
+          initialEditType="wysiwyg"
+          previewStyle="vertical"
+          class="mx-4"
+        />
+      </b-card-form>
+    </div>
+
+    <b-col cols="12">
+      <v-autocomplete
+        v-model="values"
+        :items="items"
+        dense
+        chips
+        small-chips
+        label="Solo"
+        multiple
+        solo
+      >
+      </v-autocomplete>
+    </b-col>
+
+    <b-button
+      @click="datachange"
+      class="mt-3 mx-4"
+      variant="default"
+    >
+      저장
+    </b-button>
   </div>
 </template>
 
 <script>
-export default {
+import '@toast-ui/editor/dist/toastui-editor.css'
+import 'codemirror/lib/codemirror.css'
+import { Editor } from '@toast-ui/vue-editor'
 
+export default {
+  name: 'Editor',
+    components: {
+    'editor': Editor,
+  },
+  data() {
+    return {
+      editorText: '내용을 입력해주세요',
+      editorOptions: {
+        hideModeSwitch: false
+      },
+    };
+  },
+  methods: {
+    datachange() {
+      const content = this.$refs.toastuiEditor.invoke("getMarkdown");
+      
+      this.editorText = content
+
+      console.log(this.$refs.toastuiViewer)
+      
+      this.$refs.toastuiViewer.editor.markdownValue = content
+      // console.log(this.editorText)
+      // this.editorText = (Editor.getMarkdown())
+      
+    }
+  },
 }
 </script>
 
