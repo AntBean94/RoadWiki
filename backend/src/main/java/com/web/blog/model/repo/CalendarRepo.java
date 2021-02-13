b1eb4ab84c2431aec6e518d683907c39565cd740
윤승환 <lovelyunsh@naver.com>
2021-02-13 22:19:43 +0900

    [fix] 로드맵 캘린더 cus 도 적용

--- backend/src/main/java/com/web/blog/model/repo/CalendarRepo.java
+++ backend/src/main/java/com/web/blog/model/repo/CalendarRepo.java
@@ -13,9 +13,11 @@ public interface CalendarRepo {
 	public List<Curriculum> selectBigByUid(int uid);
 	public List<Curriculum> selectMiddleByUid(int uid);
 	public List<Curriculum> selectSmallByUid(int uid);
+	public List<Curriculum> selectCusByUid(int uid);
 	
 	public int updateBig(Curriculum curriculum);
 	public int updateMiddle(Curriculum curriculum);
 	public int updateSmall(Curriculum curriculum);
+	public int updateCus(Curriculum curriculum);
 	
 }
