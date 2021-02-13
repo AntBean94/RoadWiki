152fac8e5923dfbe577e168bb91bb0e140e48ee0
whaudg98 <whaudg98@naver.com>
2021-02-13 21:46:46 +0900

    [feat] 없는 페이지 접근 할 때

--- frontend/src/views/NotFoundPage.vue
+++ frontend/src/views/NotFoundPage.vue
@@ -2,8 +2,23 @@
 <template>
   <div>
     <!-- Header -->
-    <div class="header bg-gradient-info py-7 py-lg-8 pt-lg-9">
+    <div class="header py-7 py-lg-8 pt-lg-9">
       <b-container>
+        <b-row align-h="center">
+          <h1 style="color: white">미쳤습니까 휴먼</h1>
+        </b-row>
+        <b-row align-h="center">
+          <h1 style="color: white">돌아가세욧</h1>
+        </b-row>
+        <b-row align-h="center">
+          <router-link to="/dashboard">ㅌㅌ</router-link>
+        </b-row>
+        <b-row align-h="center">
+          <img src="img/brand/pagenotfound.jpg">
+        </b-row>
+      </b-container>
+
+      <!-- <b-container>
         <div class="header-body text-center mb-7">
           <b-row class="justify-content-center">
             <b-col xl="5" lg="6" md="6" class="px-5">
@@ -14,15 +29,15 @@
             </b-col>
           </b-row>
         </div>
-      </b-container>
-      <div class="separator separator-bottom separator-skew zindex-100">
+      </b-container> -->
+      <!-- <div class="separator separator-bottom separator-skew zindex-100">
         <svg x="0" y="0" viewBox="0 0 2560 100" preserveAspectRatio="none" version="1.1" xmlns="http://www.w3.org/2000/svg">
           <polygon class="fill-default" points="2560 0 2560 100 0 100"></polygon>
         </svg>
-      </div>
+      </div> -->
     </div>
     <!-- Page content -->
-    <section class="py-6">
+    <!-- <section class="py-6">
       <b-container >
         <b-row align-v="center" class="row-grid">
           <b-col md="6">
@@ -37,7 +52,7 @@
           </b-col>
         </b-row>
       </b-container>
-    </section>
+    </section> -->
   </div>
 </template>
 
