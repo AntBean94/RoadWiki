98a0043874f71dcd096057a06fce54dbd1cfe0bd
Keonsang Yun <kesay59@gmail.com>
2021-02-15 22:20:54 +0900

    [fix] chatting

--- backend/chatting/src/main/java/com/web/blog/config/WebSocketConfig.java
+++ backend/chatting/src/main/java/com/web/blog/config/WebSocketConfig.java
@@ -12,12 +12,12 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer{
 	
 	@Override
 	public void configureMessageBroker(MessageBrokerRegistry config) {
-		config.enableSimpleBroker("/sub");
-		config.setApplicationDestinationPrefixes("/pub");
+		config.enableSimpleBroker("/chat/sub");
+		config.setApplicationDestinationPrefixes("/chat/pub");
 	}
 	
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
+		registry.addEndpoint("/chat/ws").setAllowedOriginPatterns("*").withSockJS();
 	}
 }
