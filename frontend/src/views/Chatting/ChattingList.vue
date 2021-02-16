bf71bc285b12cb1e6c141a5ea93e9b06ef7a27ae
ekals4407 <ekals4407@naver.com>
2021-02-16 18:26:42 +0900

    [Resolve] 코드 변경사항 되돌리기

--- frontend/src/views/Chatting/ChattingList.vue
+++ frontend/src/views/Chatting/ChattingList.vue
@@ -1,11 +1,8 @@
 <template>
   <div>
        <!-- <base-header class="pb-6 pb-8 pt-5 pt-md-8 bg-gradient-default"></base-header>  -->
-      <div id="chatting-list" v-for="(chatInfo, index) in chattingList" v-bind:key="index" @click="ClickchattingRoom(chatInfo)">
-      선택한 오픈채팅방
-      {{ chatInfo.roomid }}
-      {{ chatInfo.name }}
-      
+      <div v-for="(chatInfo, index) in chattingList" v-bind:key="index">
+        <ChattingItem></ChattingItem>
       </div>
       
   </div>
@@ -13,11 +10,14 @@
 
 <script>
 import store from "@/store";
-
+import ChattingItem from "../Chatting/ChattingItem"
 const SERVER_URL = store.getters.getServer;
 
 export default {
-    name: "Chatting",
+    name: "ChattingList",
+    components:{
+      ChattingItem,
+    },
     props: {
         roomid: Number,
         name: String, 
@@ -46,16 +46,26 @@ export default {
         .catch(response => {
           console.log(response);
         });
-    },
-    },
-}
+      
+        },
+      },
+  }
 </script>
 
 <style>
 #chatting-list{
   display: inline-block;
   width: 240px;
-  height: 300px;
+  height: 150px;
+  margin: 24px; 
+  overflow: hidden;
+  background-color: white;
+  /* box-shadow: 5px 5px 5px; */
+}
+#changecolor{
+  display: inline-block;
+  width: 240px;
+  height: 150px;
   margin: 24px; 
   overflow: hidden;
   background-color: white;
